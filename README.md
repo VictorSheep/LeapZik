b0cc0e2c6a38d1c907e5405c51bb7363f48a1565
Clement Teboul <teboul.clement@gmail.com>
2016-12-12 12:27:19 +0100

    Mise en place du Webpack starter

---
+++ README.md
@@ -0,0 +1,6 @@
+# LeapZik Maker
+
+## Concept
+
+Web app permettant à l'utilisateur de jouer de la musique grâce au leapMotion. En fonction des mouvements de main de l’utilisateur, un son correspondant sera généré.
+En premier lieu, l’idée serait de générer un son différent pour chaque keyTap (ou contact doigt pouce). Avec main droite pour le rythme et main gauche pour la mélodie par exemple.
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path       = require('path');
+const webpack    = require('webpack');
+
+const production = process.argv.indexOf("--production") > -1;
+
+var config = {
+  entry: "./src/main.js",
+  
+  output: {
+    path: './app',
+    publicPath: !production ? 'http://localhost:8080/' : undefined,
+    filename: 'bundle.js'
+  },
+  watch: !production,
+  debug: !production,
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets:['es2015'] }
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js'],
+  },
+
+  plugins: [],
+
+  devServer: {
+    contentBase: "./app"
+  },
+
+  jshint: { 'esversion' : 6 }
+};
+
+if (production) {
+  // Empêche UglifyJS de gueuler sur le bundle de prod
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      exclude: /node_modules/
+    })
+  );
+}
+
+module.exports = config;
